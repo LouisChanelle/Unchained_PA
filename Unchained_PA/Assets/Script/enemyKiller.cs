c54c3c02cbb38421390a2a8847f9b2c4ffef2562
LouisChanelle <57177127+LouisChanelle@users.noreply.github.com>
2020-02-13 19:24:27 +0100

    Progress

---
+++ Unchained_PA/Assets/Script/enemyKiller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class enemyKiller : MonoBehaviour
+{
+    [SerializeField] private Transform enemyPos;
+    
+    private void OnTriggerEnter (Collider other)
+    {
+        if (!Input.GetKey(KeyCode.A))
+        {
+            return;
+        }
+
+        if (other.CompareTag("enemy"))
+        {
+            Debug.Log("I'm in ! I'M IN");
+            Collider.Destroy(other.gameObject);
+        }
+    }
+}
