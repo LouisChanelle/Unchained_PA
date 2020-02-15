16810077969de7b9c88ae7b370dde4cc56749da2
LouisChanelle <57177127+LouisChanelle@users.noreply.github.com>
2020-02-15 21:07:23 +0100

    Destruction enemy

--- Unchained_PA/Assets/Script/PlayerMovement.cs
+++ Unchained_PA/Assets/Script/PlayerMovement.cs
@@ -1,27 +1,39 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
-    
+    [SerializeField] private float rotSpeed;
+    private void Start()
+    {
+        
+    }
+
     void Update()
     {
         var moveIntent = Vector3.zero;
-
+        float rotate = 0f;
+        
         if (Input.GetKey(KeyCode.D))
         {
             moveIntent += Vector3.forward;
+            rotate += 30f;
         }
 
         if (Input.GetKey(KeyCode.Q))
         {
             moveIntent += Vector3.back;
+            rotate += 30f;
         }
-
+        
+        
         moveIntent = moveIntent.normalized;
+        
 
-        transform.position += Time.deltaTime * moveSpeed * moveIntent;
+        transform.position += moveIntent * moveSpeed * Time.deltaTime;
     }
 }
